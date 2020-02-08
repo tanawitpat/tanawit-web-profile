bf770229ab93aefee3bb11f290397e6926f921a0
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-02-08 21:25:36 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Hello Tanawit!</h1>
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
