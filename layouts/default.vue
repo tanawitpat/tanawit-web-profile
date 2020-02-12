985c5ac880c83e88b02c7167a8dedd3d70491caf
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-02-13 00:30:07 +0700

    Update layout styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,20 +6,25 @@
 
 <style lang="scss">
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
   font-size: 62.5%; // 1rem = 16px;
-  margin: 0;
 
   @include respond(small) {
     font-size: 50%; //1rem = 8px, 8/16 = 50%
   }
+}
+
+body {
+  margin: 0;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.7;
+  word-spacing: 1px;
+  font-family: $font-primary;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
 }
 
 *,
