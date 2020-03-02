3f9cf7c21c4329098419c60f064d685faefe69d7
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-03-02 22:53:57 +0700

    Initialize animate on scroll

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '@/plugins/aos', ssr: false }],
   /*
    ** Nuxt.js dev-modules
    */
